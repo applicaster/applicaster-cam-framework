cca6caa50c641fd89eb3a2de3a9a7d9249718bd8
Alex Khomich <khomich@scand.com>
2019-06-04 16:40:54 +0300

    [Android] Reworked calculating padding of recycler container and items snap logic.

--- Android/cam/src/main/java/com/applicaster/cam/ui/billing/IBillingView.kt
+++ Android/cam/src/main/java/com/applicaster/cam/ui/billing/IBillingView.kt
@@ -8,6 +8,9 @@ import com.applicaster.cam.ui.billing.adapter.recycler.BillingItemType
 interface IBillingView : IBaseView {
     fun setListeners()
     fun initViewComponents(containerType: ContainerType, billingItemType: BillingItemType)
-    fun populateBillingContainer(purchaseItems: List<PurchaseItem>)
+    fun populateBillingContainer(purchaseItems: List<PurchaseItem>) {
+        calculateRecyclerContainerPadding(purchaseItems.size)
+    }
     fun clearBillingContainer()
+    fun calculateRecyclerContainerPadding(itemsCount: Int) {}
 }
