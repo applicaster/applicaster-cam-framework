f3b918cbc4db196e42198e93a74a4dfb0c318498
Alex Khomich <khomich@scand.com>
2019-07-22 18:36:13 +0300

    [Android] Added mock Analytics data provider to sample app.

---
+++ Android/sample/src/main/java/com/applicaster/camsample/mock/AnalyticsDataProviderMock.kt
@@ -0,0 +1,27 @@
+package com.applicaster.camsample.mock
+
+import com.applicaster.cam.AnalyticsDataProvider
+import com.applicaster.cam.PurchaseData
+import com.applicaster.cam.Trigger
+
+class AnalyticsDataProviderMock : AnalyticsDataProvider {
+    override fun getEntityType(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getEntityName(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getTrigger(): Trigger {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun isUserSubscribed(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getPurchaseData(): List<PurchaseData> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
