40f1a84f0e23b5ba08693c3280844d59a70f4861
Alex Khomich <khomich@scand.com>
2019-08-30 18:10:08 +0300

    [Android] Renamed onSuccess method of ActionCallback interface.

--- Android/sample/src/main/java/com/applicaster/camsample/mock/MockCamContract.kt
+++ Android/sample/src/main/java/com/applicaster/camsample/mock/MockCamContract.kt
@@ -11,37 +11,37 @@ class MockCamContract(private val context: Context) : ICamContract {
     }
 
     override fun onItemPurchased(purchase: List<Purchase>, callback: PurchaseCallback) {
-        Handler().postDelayed({ callback.onSuccess() }, 1250)
+        Handler().postDelayed({ callback.onActionSuccess() }, 1250)
     }
 
     override fun onPurchasesRestored(purchases: List<Purchase>, callback: RestoreCallback) {
-        Handler().postDelayed({ callback.onSuccess() }, 1250)
+        Handler().postDelayed({ callback.onActionSuccess() }, 1250)
     }
 
     override fun isPurchaseRequired() = true
 
     override fun login(authFieldsInput: HashMap<String, String>, callback: LoginCallback) {
-        Handler().postDelayed({ callback.onSuccess() }, 1250)
+        Handler().postDelayed({ callback.onActionSuccess() }, 1250)
     }
 
     override fun signUp(authFieldsInput: HashMap<String, String>, callback: SignUpCallback) {
-        Handler().postDelayed({ callback.onSuccess() }, 1250)
+        Handler().postDelayed({ callback.onActionSuccess() }, 1250)
     }
 
     override fun resetPassword(authFieldsInput: HashMap<String, String>, callback: PasswordResetCallback) {
-        Handler().postDelayed({ callback.onSuccess() }, 1250)
+        Handler().postDelayed({ callback.onActionSuccess() }, 1250)
     }
 
     override fun activateRedeemCode(redeemCode: String, callback: RedeemCodeActivationCallback) {
-        Handler().postDelayed({ callback.onSuccess() }, 1250)
+        Handler().postDelayed({ callback.onActionSuccess() }, 1250)
     }
 
     override fun loginWithFacebook(email: String, id: String, callback: FacebookAuthCallback) {
-        Handler().postDelayed({ callback.onSuccess() }, 1250)
+        Handler().postDelayed({ callback.onActionSuccess() }, 1250)
     }
 
     override fun signupWithFacebook(email: String, id: String, callback: FacebookAuthCallback) {
-        Handler().postDelayed({ callback.onSuccess() }, 1250)
+        Handler().postDelayed({ callback.onActionSuccess() }, 1250)
     }
 
     override fun isUserLogged() = false
